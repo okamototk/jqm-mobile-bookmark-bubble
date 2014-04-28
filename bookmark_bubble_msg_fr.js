0182a4e490e248cea5dbc54c6250cc486ae09748
Takashi Okamoto <toraneko@gmail.com>
2014-04-28 17:46:27 +0900

    Add todo about translate addtional message in French.

--- bookmark_bubble_msg_fr.js
+++ bookmark_bubble_msg_fr.js
@@ -7,6 +7,8 @@ google.bookmarkbubble.Bubble.prototype.msg = {
     '<b>Installer cette app:</b><br /> 1) Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Select "<b>Sauver dans les favoris</b>",<br /> 3) Choisir "<b>Ajouter à</b>" et "<b>Ecran d\'accueil</b>"',
   android41:
     '<b>Installer cette app:</b><br /> 1) Ajouter un favori,<br /> 2) Appuyez et maintenez le favori,<br /> 3) Select "<b>Ajouter raccourci écran d\'accueil</b>"',
+  android44:/** TODO: Please translate this messsage in French!! **/
+    '<b>Install this app:</b><br /> 1) Tap <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Select "<b>Add to homescreen</b>"',
   blackberry: 
     '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_BLACKBERRY_ICON_DATA_URL_ +'" style="height: 1em;display: inline-block;padding:0;margin:0" />, choisir "<b>Ajouter à l\'écran d\'accueil</b>"',
   playbook: 
